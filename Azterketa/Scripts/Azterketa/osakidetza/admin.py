cda25d39457a3610f2977d7f9dcf070899aa6bca
Jon Telleria <telleria.jon@uni.eus>
2024-10-14 08:38:02 +0200

    modeloak adminen sartu eta aplikazioko url-ak proiektukoan sartu

--- Azterketa/Scripts/Azterketa/osakidetza/admin.py
+++ Azterketa/Scripts/Azterketa/osakidetza/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from osakidetza.models import Paziente, Mediku, Zita
 # Register your models here.
+
+admin.site.register(Paziente)
+admin.site.register(Mediku)
+admin.site.register(Zita)
