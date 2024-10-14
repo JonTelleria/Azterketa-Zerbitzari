e4f747a9f74e0e5b8eaa3089cbffd12a113f0f35
Jon Telleria <telleria.jon@uni.eus>
2024-10-14 09:01:16 +0200

    url-ak sortu

--- Azterketa/Scripts/Azterketa/osakidetza/urls.py
+++ Azterketa/Scripts/Azterketa/osakidetza/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    
+    path('add-paziente/', views.add_paziente,name="add-paziente"),
+    path('add-mediku/', views.add_mediku,name="add-mediku"),
     ]
